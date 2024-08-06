4dc94cea980f64220ac0350551c1e0e86a395c31
PloxyMen <adilet.imakeev54@gmail.com>
2024-08-07 01:15:51 +0600

    first commit csharp

---
+++ CalsLib/Cals.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace CalsLib;
+
+public class Calc
+{
+    public static Sum(int a ,int b){
+        return a + b;
+    }
+}
---
+++ TestConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TestConsole{
+    class Program{
+        static void Main(string[] args)
+        {
+            int x = 10;
+            int y= x + 20;
+
+            Console.WriteLine("Еще привет");
+            Console.WriteLine("Hello,World");
+            
+        }
+    }
+}
